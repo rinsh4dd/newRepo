b411961c74f1684eb8dd3adac43dbdd8e5a9d8b5
rinsh4dd <rinshadcontacts@gmail.com>
2025-11-01 13:54:09 +0530

    Technician Service Updated on 01-11-2025 13:54:09.86

--- daily_log.bat
+++ daily_log.bat
@@ -1,6 +1,9 @@
 @echo off
 cd /d "C:\Users\rinsh\OneDrive\Desktop\vscode\GitAutoLogger\newRepoServices\Implementations"
-echo Auto commit at %date% %time% >> TechnicianServices.txt
+
+set LOGFILE=C:\Users\rinsh\OneDrive\Desktop\vscode\GitAutoLogger\newRepoServices\Implementations\TechnicianServices.txt
+
+echo Auto commit at %date% %time% >> "%LOGFILE%"
 git add .
 git commit -m "Technician Service Updated on %date% %time%"
 git push origin main
